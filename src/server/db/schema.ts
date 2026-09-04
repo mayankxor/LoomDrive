8d19cb248d4d3dd0449c1d13c9e3dbcf8e5c1711
Mayank Rajput <mayankxor@gmail.com>
2026-09-04 20:57:49 +0000

    Updated schema for files and folders

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,19 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import { int, text, singlestoreTable, index } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
+export const files = singlestoreTable("files_table", {
   id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+  name: text("name").notNull(),
+  size: int("size").notNull(),
+  url: text("url").notNull(),
+  parent: int("parent").notNull(),
+}, (t) => {
+  return [index("parent_index").on(t.parent)];
+})
+
+export const folders = singlestoreTable("folders_table", {
+  id: int("id").primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: int("parent"),
+}, (t) => {
+  return [index("parent_index").on(t.parent)];
+})
